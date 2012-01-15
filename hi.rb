8bb3dac798e09f1f5047ddae514d1435e9d63081
John C. Dages <John.Dages@collaborativeit.com>
2012-01-15 15:54:42 -0500

    Dependencies

---
+++ hi.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'sinatra'
+require 'erb'
+require 'json'
+
+get '/:something' do
+  @lol = params['something']
+  erb :index,
+end
+
+get '/data/' do
+  {:john => 'first name', :dages => 'last name'}.to_json
+end
+
+get '/test/:testVariable' do
+  @lol = params[:testVariable]
+  '<h1>' + @lol + '</h1>'
+end
+
